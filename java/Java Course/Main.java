e2675750e69a8356fe5ea130d6256c90d51aa6ac
Panha <turtoisecool@gmail.com>
2023-12-01 19:06:46 +0700

    fshgsjdfjldj

---
+++ java/Java Course/Main.java
@@ -0,0 +1,13 @@
+public class Main{
+    public static void main(String[] args){
+        int num = 69;
+        String Str = "A";
+        System.out.print("Hah ");
+        System.out.println("Hello world");
+
+        System.out.println(num);
+        System.out.println(Str);
+        System.out.println("String");
+
+    }
+}
